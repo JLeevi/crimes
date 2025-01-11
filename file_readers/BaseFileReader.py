f428c1ab646316bb14f63e0f6705f06aa5377366
Leevi Jäkälä <jakala.leevi@gmail.com>
2025-01-11 17:35:28 +0100

    convert all float columns to int
    the id column numbers where represented as floats, which was annoying

--- file_readers/BaseFileReader.py
+++ file_readers/BaseFileReader.py
@@ -9,13 +9,24 @@ class BaseFileReader:
             os.path.dirname(__file__), "../data/CA/")
 
     def read_as_dataframe(self, file_name: str):
-        return pd.read_csv(self.__to_full_path(file_name))
+        df = pd.read_csv(self.__to_full_path(file_name))
+        df = self.__convert_id_columns_to_integers(df)
+        return df
 
     def merge_dfs(self, first_df: pd.DataFrame, second_df: pd.DataFrame, on_column: str):
         columns_without_duplicates = self.__get_columns_without_duplicates(
             first_df, second_df, on_column)
         second_df_without_duplicate_columns = second_df[columns_without_duplicates]
-        return pd.merge(first_df, second_df_without_duplicate_columns, on=on_column, how="outer")
+        merged_df = pd.merge(
+            first_df, second_df_without_duplicate_columns, on=on_column, how="outer")
+        merged_df = self.__convert_id_columns_to_integers(merged_df)
+        return merged_df
+
+    def __convert_id_columns_to_integers(self, df: pd.DataFrame):
+        float_columns = df.select_dtypes(include=["float"]).columns
+        df[float_columns] = df[float_columns].astype("Int64")
+        df.head()
+        return df
 
     def __to_full_path(self, file):
         return self.__folder_path + file
